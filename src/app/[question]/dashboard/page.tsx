a12ef6a0cebac19b7e9a9f3df04fdb3a5cbbe575
ritu-476 <ritujoharbishnoi@gmail.com>
2025-02-17 16:25:09 +0530

    done

--- src/app/[question]/dashboard/page.tsx
+++ src/app/[question]/dashboard/page.tsx
@@ -3,16 +3,20 @@
 import { useParams } from "next/navigation";
 import QuestionOne from "@/components/QuestionOne"
 import QuestionTwo from "@/components/QuestionTwo";
+import React, { Suspense } from 'react'
+
 
 
 const page = () => {
   const params = useParams();
   const { question } = params;
   return (
-    <>
-      {question === "question-1" && <QuestionOne />}
-      {question === "question-2" && <QuestionTwo />}
-    </>
+  
+     <Suspense>
+     {question === "question-1" && <QuestionOne />}
+     {question === "question-2" && <QuestionTwo />}        </Suspense>
+     
+ 
   );
 };
 
